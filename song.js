9e088a0bff8ac8f54cc772a47dfa06705ff4e29b
kyawminhtwe <kyawminhtwe107@gmail.com>
2024-08-26 16:16:56 +0630

    first commit

---
+++ song.js
@@ -0,0 +1,30 @@
+const songs = [
+	{
+		id: 1,
+		title: 'Sunflower Street Drumloop 85bpm',
+		name: 'Kamhunt',
+		song: 'song/song1.mp3',
+		cover: 'img/img1.webp'
+	},
+	{
+		id: 2,
+		title: '[Intro Music] BLACK BOX - Cymatics VS Slate digi',
+		name: 'Black Box',
+		song: 'song/song2.mp3',
+		cover: 'img/img2.jpeg'
+	},
+	{
+		id: 3,
+		title: 'kick hardstyle',
+		name: 'Van Wiese',
+		song: 'song/song3.mp3',
+		cover: 'img/img3.jpeg'
+	},
+	{
+		id: 4,
+		title: 'Nodens (Field Song)',
+		name: 'Pixabay',
+		song: 'song/song4.mp3',
+		cover: 'img/img4.jpeg'
+	}
+]
